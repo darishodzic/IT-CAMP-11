f65b43a788c6f5b3be332691fc531ed513f75441
daris <daris.hodzic.dh@gmail.com>
2022-08-02 21:02:25 +0200

    commit

---
+++ vezba18/app.js
@@ -0,0 +1,51 @@
+//DATA TYPE / Primitivni i referentin tipovi
+//Primitivni tipovi
+
+//Number
+//1 2 6 10 -3 -7 2.5 -2.5
+
+//String
+// "Hello World"   'Daris'
+
+//Boolean
+//true false
+
+// vrednosnost = 10;
+
+// console.log(vrednosnost);
+// console.log(vrednosnost);
+
+// vrednosnost = 2;
+
+// console.log(vrednosnost);
+// console.log(vrednosnost);
+
+// console.log(3 + 2);
+// console.log(3 - 2);
+// console.log(3 * 2);
+// console.log(3 / 2);
+// console.log(3 ** 2);
+// console.log(3 % 2);
+
+// a = 5;
+
+// b = 3;
+
+// c = a + b;
+
+// console.log(c);
+
+// a = "Hello";
+
+// b = "World";
+
+// console.log(a + b);
+
+// a = 3;
+
+// b = 5;
+
+// console.log(b == a);
+
+//Logicke operacije
+//< > == === <= >= != !==
