efc77dacfc87f858b5e86ffe43bcf204f89313ee
daris <daris.hodzic.dh@gmail.com>
2022-08-04 20:49:36 +0200

    comit

--- vezba18/app.js
+++ vezba18/app.js
@@ -49,3 +49,56 @@
 
 //Logicke operacije
 //< > == === <= >= != !==
+
+// //falsy values
+// 0;
+// ("");
+// null;
+// undefined;
+// NaN;
+// false;
+
+// console.log("PRE IFA");
+
+// if (0) {
+//   console.log("Daris");
+// }
+
+// console.log("POSLE IFA");
+
+// unos1 = Number(prompt("Unesi broj"));
+// unos2 = Number(prompt("Unesi broj"));
+
+// if (unos1 === unos2) {
+//   console.log("BROJEVI SU JEDNAKI");
+// } else {
+//   console.log("BROJEVI NISU JEDNAKI");
+// }
+
+unos1 = Number(prompt("Unesi broj 1."));
+unos2 = Number(prompt("Unesi broj 2."));
+
+if (unos1 > unos2) {
+  prvi = unos1;
+  drugi = unos2;
+} else {
+  prvi = unos2;
+  drugi = unos1;
+}
+
+unos3 = Number(prompt("Unesi broj 3."));
+
+if (unos3 > prvi) {
+  treci = drugi;
+  drugi = prvi;
+  prvi = unos3;
+} else if (unos3 < drugi) {
+  treci = unos3;
+} else {
+  treci = drugi;
+  drugi = unos3;
+}
+
+console.log("1. broj je " + prvi);
+console.log("2. broj je " + drugi);
+console.log("3. broj je " + treci);
