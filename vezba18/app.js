3d8650418124ce5e3f14cb5f930e572a3fd10bc3
daris <daris.hodzic.dh@gmail.com>
2022-08-08 20:57:50 +0200

    commit

--- vezba18/app.js
+++ vezba18/app.js
@@ -75,30 +75,85 @@
 //   console.log("BROJEVI NISU JEDNAKI");
 // }
 
-unos1 = Number(prompt("Unesi broj 1."));
-unos2 = Number(prompt("Unesi broj 2."));
+// unos1 = Number(prompt("Unesi broj 1."));
+// unos2 = Number(prompt("Unesi broj 2."));
 
-if (unos1 > unos2) {
-  prvi = unos1;
-  drugi = unos2;
-} else {
-  prvi = unos2;
-  drugi = unos1;
+// if (unos1 > unos2) {
+//   prvi = unos1;
+//   drugi = unos2;
+// } else {
+//   prvi = unos2;
+//   drugi = unos1;
+// }
+
+// unos3 = Number(prompt("Unesi broj 3."));
+
+// if (unos3 > prvi) {
+//   treci = drugi;
+//   drugi = prvi;
+//   prvi = unos3;
+// } else if (unos3 < drugi) {
+//   treci = unos3;
+// } else {
+//   treci = drugi;
+//   drugi = unos3;
+// }
+
+// console.log("1. broj je " + prvi);
+// console.log("2. broj je " + drugi);
+// console.log("3. broj je " + treci);
+
+// console.log("PRE FORA");
+
+// for (i = 5; i <= 1000; i++) {
+//   console.log(i);
+// }
+
+// console.log("POSLE FORA");
+
+// sum = 0;
+// end = Number(prompt("Unesi broj"));
+
+// for (i = 1; i <= end; i++) {
+//   sum += i;
+// }
+
+// console.log(sum);
+
+// sum = 0;
+// avr = 0;
+
+// for (i = 1; i <= 10; i++) {
+//   unos = Number(prompt("Unesi broj"));
+//   sum += unos;
+// }
+
+// console.log("zbir je " + sum);
+// console.log("prosek je " + sum / 10);
+
+// end = Number(prompt("Unesi broj"));
+
+// for (i = 1; i <= end; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+end = Number(prompt("Unesi broj"));
+
+for (i = 1; i <= end; i++) {
+  console.log("Cube of " + i + " is " + i ** 3);
 }
 
-unos3 = Number(prompt("Unesi broj 3."));
-
-if (unos3 > prvi) {
-  treci = drugi;
-  drugi = prvi;
-  prvi = unos3;
-} else if (unos3 < drugi) {
-  treci = unos3;
-} else {
-  treci = drugi;
-  drugi = unos3;
+for (i = 0; i < 5; i++) {
+  console.log(i);
+  for (j = 0; j < 5; j++) {
+    console.log(j);
+  }
 }
-
-console.log("1. broj je " + prvi);
-console.log("2. broj je " + drugi);
-console.log("3. broj je " + treci);
